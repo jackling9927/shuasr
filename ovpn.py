5a61506452be8416abd72624a5a02afad052af0d
panghaibin <panghaibin@shu.edu.cn>
2022-03-10 20:30:11 +0800

    GitHub test

--- ovpn.py
+++ ovpn.py
@@ -1,4 +1,15 @@
 import os
+SHU_DOMAINS = [
+    'speedtest.shu.edu.cn',
+    'selfreport.shu.edu.cn',
+    'newsso.shu.edu.cn',
+    'xk.autoisp.shu.edu.cn',
+]
+
+
+def get_ip(domain):
+    ip = os.popen("dig +short {}".format(domain)).read().strip()
+    return ip
 
 
 def config_ovpn():
@@ -11,9 +22,10 @@ def config_ovpn():
     routing_config = """route-nopull
     route-metric 150
     max-routes 1000
-    route 202.120.127.100 255.255.255.255
-    route 202.120.127.250 255.255.255.255
     """
+    for domain in SHU_DOMAINS:
+        routing_config += "route %s 255.255.255.255" % get_ip(domain)
+
     with open('.github/vpn/config.ovpn', 'r') as f:
         content = f.read()
         content = content.replace('# ROUTING CONFIG', routing_config)
